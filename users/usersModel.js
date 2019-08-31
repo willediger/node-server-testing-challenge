d46368afa88c6ee1330d4f39533615a6bc0a962e
unknown <will.ediger@gmail.com>
2019-08-31 18:41:17 -0500

    added del function

--- users/usersModel.js
+++ users/usersModel.js
@@ -4,7 +4,8 @@ module.exports = {
   add,
   find,
   findBy,
-  findById
+  findById,
+  del
 };
 
 function find() {
@@ -19,6 +20,14 @@ async function add(user) {
   const [id] = await db("users").insert(user);
 
   return findById(id);
+}
+
+async function del(id) {
+  const user = await findById(id);
+  await db("users")
+    .where("id", "=", id)
+    .del();
+  return user;
 }
 
 function findById(id) {
